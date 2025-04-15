143b4ce000308df47a80871bfda6c8796477167f
Bear Kennedy <196842211+BearKennedy@users.noreply.github.com>
2025-04-15 14:19:13 +0000

    Getting formatting down further
    1. Updated documentation do be more readable with
    multiline comments for important things and code
    unused code marked out with single line comments.
    
    2.change API output from *Metric* to *Imperial*

--- DistanceMatrix.py
+++ DistanceMatrix.py
@@ -1,39 +1,97 @@
-from googlemaps import convert
+from googlemaps import convert ## idk if this is really needed 
 import googlemaps
-import os
 
-def calculate_distance_matrix(api_key, origins, destinations,mode=None, language=None, avoid=None, units=None,
-                    departure_time=None, arrival_time=None, transit_mode=None,
-                    transit_routing_preference=None, traffic_model=None, region=None):
+# import os 
+"""""/\
+this is only used to hide key,the function is on line #118 and involves
+storing the key on your system, idk if we really want that 
+"""
+#Gets travel distance and time for a matrix of origins and destinations.
+def calculate_distance_matrix(api_key, origins, destinations,mode=None,
+                              language=None, avoid=None, units="imperial",
+                              departure_time=None, arrival_time=None, transit_mode=None,
+                              transit_routing_preference=None, traffic_model=None, region=None):
+    
+    """ Just documentation from the repo that we can use. it states all parameters above, 
+    we can change them as needed.
+    
+    Gets travel distance and time for a matrix of origins and destinations.
+
+   { :param origins: One or more addresses, Place IDs, and/or latitude/longitude
+        values, from which to calculate distance and time. Each Place ID string
+        must be prepended with 'place_id:'. If you pass an address as a string,
+        the service will geocode the string and convert it to a
+        latitude/longitude coordinate to calculate directions.
+        
+    :type origins: a single location, or a list of locations, where a
+        location is a string, dict, list, or tuple   }
+
+    { :param destinations: One or more addresses, Place IDs, and/or lat/lng values,
+        to which to calculate distance and time. Each Place ID string must be
+        prepended with 'place_id:'. If you pass an address as a string, the
+        service will geocode the string and convert it to a latitude/longitude
+        coordinate to calculate directions.
+        
+    :type destinations: a single location, or a list of locations, where a
+        location is a string, dict, list, or tuple }
+
+    { :param mode: Specifies the mode of transport to use when calculating
+        directions. Valid values are "driving", "walking", "transit" or
+        "bicycling".
+        
+    :type mode: string }
+
+   { :param language: The language in which to return results.
+   
+    :type language: string   }
+
+   { :param avoid: Indicates that the calculated route(s) should avoid the
+        indicated features. Valid values are "tolls", "highways" or "ferries".
+        
+    :type avoid: string   }
+
+    { :param units: Specifies the unit system to use when displaying results.
+        Valid values are "metric" or "imperial".
+        
+    :type units: string }
+
+    { :param departure_time: Specifies the desired time of departure.
+    
+    :type departure_time: int or datetime.datetime  }
+
+   { :param arrival_time: Specifies the desired time of arrival for transit
+        directions. Note: you can't specify both departure_time and
+        arrival_time.
+    :type arrival_time: int or datetime.datetime }
+
+    { :param transit_mode: Specifies one or more preferred modes of transit.
+        This parameter may only be specified for requests where the mode is
+        transit. Valid values are "bus", "subway", "train", "tram", "rail".
+        "rail" is equivalent to ["train", "tram", "subway"].
+        
+    :type transit_mode: string or list of strings  }
+
+    { :param transit_routing_preference: Specifies preferences for transit
+        requests. Valid values are "less_walking" or "fewer_transfers".
+        
+    :type transit_routing_preference: string }
+
+    :param traffic_model: Specifies the predictive travel time model to use.
+        Valid values are "best_guess" or "optimistic" or "pessimistic".
+        The traffic_model parameter may only be specified for requests where
+        the travel mode is driving, and where the request includes a
+        departure_time.
+
+    { :param region: Specifies the prefered region the geocoder should search
+        first, but it will not restrict the results to only this region. Valid
+        values are a ccTLD code.
+        
+    :type region: string  }
+
+    :rtype: matrix of distances. Results are returned in rows, each row
+        containing one origin paired with each destination.
     """
-    Calculates the distance and duration between multiple origins and destinations using the Google Distance Matrix API.
 
-    Args:
-        api_key (str): Your Google Maps Platform API key.
-        origins (list of str or tuples): One or more locations to travel from. Specify as strings (addresses or place IDs)
-                                         or as (latitude, longitude) tuples.
-        destinations (list of str or tuples): One or more destination locations. Specify as strings (addresses or place IDs)
-                                            or as (latitude, longitude) tuples.
-        mode (str, optional): The travel mode to use when calculating directions.
-                             Valid values are "driving", "walking", "bicycling", or "transit". Defaults to None (driving).
-        transit_options (dict, optional): Specifies options that are only valid for transit requests. See Google Maps API documentation for details.
-        avoid (str or list of str, optional): Restrictions to place on the request. Valid values are:
-                                              "tolls", "highways", "ferries", "indoor". Defaults to None.
-        units (str, optional): Specifies the unit system to use when displaying results.
-                              Valid values are "metric" or "imperial". Defaults to None (API default).
-        language (str, optional): The language in which to return results. See Google Maps API documentation for supported languages.
-        region (str, optional): The region code, specified as a ccTLD ("top-level domain") two-character value.
-        departure_time (datetime or int, optional): Specifies the desired time of departure as a datetime object or seconds since midnight, January 1, 1970 UTC.
-        arrival_time (datetime or int, optional): Specifies the desired time of arrival for transit requests as a datetime object or seconds since midnight, January 1, 1970 UTC.
-        travel_mode (str, optional): Specifies the mode of transport to use in transit requests. Valid values are "bus", "rail", "subway", "train", "tram".
-        driving_options (dict, optional): Specifies options that are only valid for driving requests. See Google Maps API documentation for details.
-        traffic_model (str, optional): Specifies the assumptions to use when calculating time in traffic. Valid values are "best_guess", "pessimistic", "optimistic". Only applicable when `mode` is "driving" and `departure_time` is specified.
-        optimize_waypoint_order (bool, optional): If set to True, the Directions service will attempt to re-order the supplied waypoints to minimize the overall cost of the route. Only applicable when there is more than one waypoint. Defaults to False.
-
-    Returns:
-        dict: A dictionary representing the JSON response from the Distance Matrix API.
-              Returns None if an error occurs.
-    """
     try:
         gmaps = googlemaps.Client(key=api_key)
         result = gmaps.distance_matrix(
@@ -46,11 +104,11 @@ def calculate_distance_matrix(api_key, origins, destinations,mode=None, language
             region=region,
             departure_time=departure_time,
             arrival_time=arrival_time,
-           # travel_mode=travel_mode, was fucking up code 
             transit_mode = transit_mode,
             traffic_model=traffic_model,
            transit_routing_preference= transit_routing_preference
         )
+        # error handling 
         return result
     except googlemaps.exceptions.ApiError as e:
         print(f"An API error occurred: {e}")
@@ -60,36 +118,46 @@ def calculate_distance_matrix(api_key, origins, destinations,mode=None, language
         return None
     
 if __name__ == "__main__":
+    
     # Replace with your actual Google Maps Platform API key
-   # api_key = os.environ.get("GOOGLE_MAPS_API_KEY") #WHAT THE FUCK IS THAT
+   # api_key = os.environ.get("GOOGLE_MAPS_API_KEY")
+   
     api_key = "AIzaSyC2UHZ50CgCeueKLSZsp5NS7rJnUrj38gA"
     if not api_key:
         print("Please set the GOOGLE_MAPS_API_KEY environment variable.")
     else:
-        ## TESTING since this worked
+        
+        """""
+        ## We can pass origins in this format or via coordinates, currently set up for coords
+        # But if we go back to the names we have to change the printing variables.
         """""
         # Define your origins and destinations
-        origins = ["Hartland, MI", "Brighton, MI"]
-        destinations = ["Ann Arbor, MI", "Novi, MI"]
-        """
+        #origins = ["Hartland, MI", "Brighton, MI"]
+        #destinations = ["Ann Arbor, MI", "Novi, MI"]
+        
        
         origins_coords = [(42.6017, -83.6807),  # Example: Hartland, MI coordinates
-                  (42.5336, -83.7828)]  # Example: Brighton, MI coordinates
+                         (42.5336, -83.7828)]  # Example: Brighton, MI coordinates
 
         destinations_coords = [(42.2808, -83.7430),  # Example: Ann Arbor, MI coordinates
-                       (42.4817, -83.4758)]
+                              (42.4817, -83.4758)]
 
 ## ////////////////////////////////////////////////////////// not needed?
         # Optional parameters (you can customize these)
        # travel_mode = "driving"
        # units = "imperial"  # or "metric"
 #/////////////////////////////////////////////////////
-        # Calculate the distance matrix
+        """""
+        Calculate the distance matrix by calling the function
+        Since we everything but api key, origins and destinations have default values
+        in 
+        """
     distance_matrix_result = calculate_distance_matrix(
             api_key=api_key,
             origins=origins_coords,
             destinations=destinations_coords,
         )
+    #print(distance_matrix_result) ///////// use this if you want to see default output
 
         # Print the results
     if distance_matrix_result:
